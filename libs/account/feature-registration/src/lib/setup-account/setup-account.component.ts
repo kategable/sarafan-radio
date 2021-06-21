6b66261f53b74d897cc1653b07f3e1cd5e7ae3ba
Kate Sky <kateskytech@gmail.com>
2021-06-21 12:56:54 -0500

    added same as prev time check added

--- libs/account/feature-registration/src/lib/setup-account/setup-account.component.ts
+++ libs/account/feature-registration/src/lib/setup-account/setup-account.component.ts
@@ -1,13 +1,28 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Store } from '@ngrx/store';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 import * as AccountActions from './../+state/account.actions';
-import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
-import {MatChipInputEvent} from '@angular/material/chips';
+import {
+  MatAutocompleteSelectedEvent,
+  MatAutocomplete,
+} from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
 import { of } from 'rxjs';
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(
@@ -26,7 +41,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 @Component({
   selector: 'account-setup-account',
   templateUrl: './setup-account.component.html',
-  styleUrls: ['./setup-account.component.scss']
+  styleUrls: ['./setup-account.component.scss'],
 })
 export class SetupAccountComponent implements OnInit {
   @ViewChild('serviceInput') serviceInput: ElementRef<HTMLInputElement>;
@@ -47,11 +62,11 @@ export class SetupAccountComponent implements OnInit {
   secondFormGroup: FormGroup = this._formBuilder.group({
     addressCtrl: ['', Validators.required],
     descCtrl: ['', Validators.required],
-    serviceCtrl: ['']
+    serviceCtrl: [''],
   });
 
   get serviceCtrl(): FormControl {
-    return this.secondFormGroup.get("serviceCtrl") as FormControl;
+    return this.secondFormGroup.get('serviceCtrl') as FormControl;
   }
 
   thirdFormGroup: FormGroup = this._formBuilder.group({
@@ -59,26 +74,61 @@ export class SetupAccountComponent implements OnInit {
   });
 
   week = {
-    name: 'Weekdays',
-    completed: false,
-    subtasks: [
-      { name: 'Monday', completed: false, color: 'primary' },
-      { name: 'Tuesday', completed: false, color: 'primary' },
-      { name: 'Wednesday', completed: false, color: 'primary' },
-      { name: 'Thursday', completed: false, color: 'primary' },
-      { name: 'Friday', completed: false, color: 'primary' },
-    ],
-  };
-  weekend = {
-    name: 'Weekend',
-    completed: false,
-    subtasks: [
-      { name: 'Saturday', completed: false, color: 'primary' },
-      { name: 'Saturday', completed: false, color: 'primary' },
-    ],
-  };
+    days: [
+    {
+      name: 'Monday',
+      completed: false,
+      color: 'primary',
+      startTime: '9:00 AM',
+      endTime: ''
+    },
+    {
+      name: 'Tuesday',
+      completed: false,
+      color: 'primary',
+      startTime: '',
+      endTime: '',
+    },
+    {
+      name: 'Wednesday',
+      completed: false,
+      color: 'primary',
+      startTime: '',
+      endTime: '',
+    },
+    {
+      name: 'Thursday',
+      completed: false,
+      color: 'primary',
+      startTime: '',
+      endTime: ''
+    },
+    {
+      name: 'Friday',
+      completed: false,
+      color: 'primary',
+      startTime: '',
+      endTime: ''
+    },
+    {
+      name: 'Saturday',
+      completed: false,
+      color: 'primary',
+      startTime: '',
+      endTime: ''
+    },
+    {
+      name: 'Sunday',
+      completed: false,
+      color: 'primary',
+      startTime: '',
+      endTime: ''
+    }
+  ] };
+
   allComplete: boolean = false;
   separatorKeysCodes: number[] = [ENTER, COMMA];
+
   todos = [
     { name: 'Woman Haircut', image: './assets/unDraw/jewelry_designer.svg' },
     { name: 'Kids Haircut', image: './assets/unDraw/jewelry_designer.svg' },
@@ -89,100 +139,89 @@ export class SetupAccountComponent implements OnInit {
     { name: 'Realtor', image: './assets/unDraw/Realtor.svg' },
   ];
 
-  services=[]
-  constructor(private store: Store,
-    private _formBuilder: FormBuilder,) { }
+  services = [];
+  constructor(private store: Store, private _formBuilder: FormBuilder) {}
 
-    ngOnInit() {
-      this.mainFormGroup = this._formBuilder.group({
-        firstFormGroup: this.firstFormGroup,
-        secondFormGroup: this.secondFormGroup,
-        thirdFormGroup: this.thirdFormGroup,
-      });
-      this.load();
+  ngOnInit() {
+    this.mainFormGroup = this._formBuilder.group({
+      firstFormGroup: this.firstFormGroup,
+      secondFormGroup: this.secondFormGroup,
+      thirdFormGroup: this.thirdFormGroup,
+    });
+    this.load();
+  }
 
+  load(): void {
+    let data: any = JSON.parse(localStorage.getItem('formdata'));
+    if (data) {
+      this.mainFormGroup.patchValue(data.form);
+      this.services = data.services || [];
+      this.week = data.schedule;
+    }
+    console.log(this.week);
+
+  }
+
+  done = [];
+  todos$ = of(this.todos);
+
+  submit() {
+    let storage = {
+      form: this.mainFormGroup.value,
+      services: this.services,
+      schedule: this.week
+    };
+
+    localStorage.setItem('formdata', JSON.stringify(storage));
+    this.store.dispatch(
+      AccountActions.create({ account: { name: 'test', id: null } })
+    );
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.todos.push({ name: value, image: '' });
     }
 
-    load(): void {
+    // Clear the input value
+    event.chipInput!.clear();
 
-      let data: any = JSON.parse(localStorage.getItem('formdata'));
-      if(data)  {
-        this.mainFormGroup.patchValue(data.form);
-        this.services = data.services || [];
-      }
+    this.serviceCtrl.setValue(null);
+  }
+
+  remove(value: string): void {
+    const index = this.services.findIndex((t) => t.name === value);
+
+    if (index >= 0) {
+      this.services.splice(index, 1);
     }
-    updateAllComplete() {
-      this.allComplete =
-        this.week.subtasks != null &&
-        this.week.subtasks.every((t) => t.completed);
-    }
+  }
 
-    someComplete(): boolean {
-      if (this.week.subtasks == null) {
-        return false;
-      }
-      return (
-        this.week.subtasks.filter((t) => t.completed).length > 0 &&
-        !this.allComplete
-      );
-    }
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.services.push({ name: event.option.viewValue, image: '' });
+    this.serviceInput.nativeElement.value = '';
+    this.serviceCtrl.setValue(null);
+  }
+  setTime(i: number){
+    this.week.days[i].startTime = this.week.days[i-1].startTime;
+    this.week.days[i].endTime = this.week.days[i-1].endTime;
+    this.week.days[i].completed = true;
 
-    setAll(completed: boolean) {
-      this.allComplete = completed;
-      if (this.week.subtasks == null) {
-        return;
-      }
-      this.week.subtasks.forEach((t) => (t.completed = completed));
-    }
+  }
+  setStartTime(e, i: number){
+    this.week.days[i].startTime = e;
+    this.week.days[i].completed = true;
+  }
+  setEndTime(e, i: number){
+    this.week.days[i].endTime = e;
+  }
+  // private _filter(value: string): string[] {
+  //  const filterValue = value.toLowerCase();
 
-    done = [];
-    todos$ = of(this.todos);
+  // return this.allFruits.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
 
-    submit() {
-      let storage = {
-        form: this.mainFormGroup.value,
-        services: this.services
-      };
-
-      localStorage.setItem('formdata',JSON.stringify( storage) );
-      this.store.dispatch(
-        AccountActions.create({ account: { name: "test", id: null } })
-      );
-    }
-
-    add(event: MatChipInputEvent): void {
-      const value = (event.value || '').trim();
-
-      // Add our fruit
-      if (value) {
-        this.todos.push({name:value, image:''});
-      }
-
-      // Clear the input value
-      event.chipInput!.clear();
-
-      this.serviceCtrl.setValue(null);
-    }
-
-    remove(value: string): void {
-      const index = this.todos.findIndex(t=>t.name === value);
-      console.log('chip remove',index);
-
-      if (index >= 0) {
-        this.todos.splice(index, 1);
-      }
-    }
-
-    selected(event: MatAutocompleteSelectedEvent): void {
-      this.services.push({name: event.option.viewValue, image: ''});
-      this.serviceInput.nativeElement.value = '';
-      this.serviceCtrl.setValue(null);
-    }
-
-   // private _filter(value: string): string[] {
-    //  const filterValue = value.toLowerCase();
-
-     // return this.allFruits.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
-
-   // }
+  // }
 }
