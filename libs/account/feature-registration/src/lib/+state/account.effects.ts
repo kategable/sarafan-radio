1a0f40d25d41fcf423e38d7c4a2c7aba06916c8b
Kate Sky <kateskytech@gmail.com>
2021-11-20 14:05:11 -0600

    effect undo

--- libs/account/feature-registration/src/lib/+state/account.effects.ts
+++ libs/account/feature-registration/src/lib/+state/account.effects.ts
@@ -8,39 +8,52 @@ import { Router } from '@angular/router';
 
 @Injectable()
 export class AccountEffects {
-  init$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(AccountActions.init),
-      fetch({
-        run: (action) => {
-          // Your custom service 'load' logic goes here. For now just return a success action...
-          return AccountActions.loadAccountSuccess({ account: [] });
-        },
+  // load$ = createEffect(() =>
+  //   this.actions$.pipe(
+  //     ofType(ProvidersActions.loadProviders),
+  //     switchMap(() =>
+  //       this.providerDataService.load().pipe(
+  //         map((i) => ProvidersActions.loadProvidersSuccess({ providers: i })),
+  //         catchError(async (error) => ProvidersActions.loadProvidersFailure({ error }))
+  //       )
+  //     )
+  //   )
+  // );
 
-        onError: (action, error) => {
-          console.error('Error', error);
-          return AccountActions.loadAccountFailure({ error });
-        },
-      })
-    )
-  );
-  createAccount$ = createEffect(() =>
-  this.actions$.pipe(
-    ofType(AccountActions.create),
-    fetch({
-      run: (action) => {
-     
-        // Your custom service 'load' logic goes here. For now just return a success action...
-        this.router.navigate(['/account']);
-        return AccountActions.createAccountSuccess({ account: action.account });
-      },
 
-      onError: (action, error) => {
-        console.error('Error', error);
-        return AccountActions.createAccountFailure({ error });
-      },
-    })
-  )
-);
+  // init$ = createEffect(() =>
+  //   this.actions$.pipe(
+  //     ofType(AccountActions.init),
+  //     fetch({
+  //       run: (action) => {
+  //         // Your custom service 'load' logic goes here. For now just return a success action...
+  //         return AccountActions.loadAccountSuccess({ account: [] });
+  //       },
+
+  //       onError: (action, error) => {
+  //         console.error('Error', error);
+  //         return AccountActions.loadAccountFailure({ error });
+  //       },
+  //     })
+  //   )
+  // );
+  // createAccount$ = createEffect(() =>
+  // this.actions$.pipe(
+  //   ofType(AccountActions.create),
+  //   fetch({
+  //     run: (action) => {
+
+  //       // Your custom service 'load' logic goes here. For now just return a success action...
+  //       this.router.navigate(['/account']);
+  //       return AccountActions.createAccountSuccess({ account: action.account });
+  //     },
+
+  //     onError: (action, error) => {
+  //       console.error('Error', error);
+  //       return AccountActions.createAccountFailure({ error });
+  //     },
+  //   })
+  // )
+//);
   constructor(private actions$: Actions,private router: Router) {}
 }
